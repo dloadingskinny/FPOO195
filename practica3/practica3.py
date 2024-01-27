cd90ff908120e27d966607b4040cc14e624999ab
dloadingskinny <Daniel.y.g.a@hotmail.com>
2024-01-26 22:33:46 -0600

    practica 3

--- practica3/practica3.py
+++ practica3/practica3.py
@@ -6,8 +6,39 @@ print(T1)
 print("se te pagara",b)"""
 
 #2 
+nombre = input("Introduce tu nombre completo: ")
 
-var1=(str(input("¿Cual es tu nombre completo?")))
-print(lower(var1))
-print(upper(var1))
-print(var1)
+print("En minúsculas:", nombre.lower())
+print("En mayúsculas:", nombre.upper())
+print("Primera letra en mayúscula:", nombre.title())
+
+#3
+x = int(input("Introduce un número entero X: "))
+suma = sum(range(1, x + 1))
+print("La suma de todos los enteros desde 1 hasta", x, "es:", suma)
+#4
+n = input("Introduce tu nombre: ")
+
+n_upper = n.upper()
+n_letras = len(n)
+
+print(f"{n_upper} tiene {n_letras} letras.")
+
+#5
+p = int(input("Número de payasos vendidos: "))
+m = int(input("Número de muñecas vendidas: "))
+
+pp = 112  
+pm = 75   
+
+pt = (p * pp) + (m * pm)
+
+print(f"Peso total del paquete: {pt} g")
+
+#6
+frase = input("Introduce una frase: ")
+
+frase_invertida = frase[::-1]
+
+print("Frase invertida:", frase_invertida)
+
