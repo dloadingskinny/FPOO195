d1b6fe2207e32373c027754c5823e6e7a9989993
dloadingskinny <Daniel.y.g.a@hotmail.com>
2024-02-07 19:39:52 -0600

    ejercicio 2

---
+++ exam_1p/2do_prob.py
@@ -0,0 +1,15 @@
+#programa que genere la serie de collatz de un numero entero solicitado de un numero entro solicitado al usuario se sontruye de la siguiente forma:
+#si el numero es par se lo divide entre dos;si es impar, se multiplica por tres y se le suma uno;la serie termina al llegar a uno
+
+def numEnt(n): 
+  serie = [n]
+  while n != 1:
+    if n % 2 == 0:
+      n //= 2
+    else:
+      n = 3 * n + 1
+    serie.append(n)
+  return serie
+n = int(input("Ingrese cualquier número entero: "))
+serie = numEnt(n)
+print("nuestra serie de Collatz de {} es: {}".format(n, serie))
--- funciones/practica6.py
+++ funciones/practica6.py
@@ -1,3 +1,4 @@
+#1
 def calc_iva(cant_sniva, porcentaje_iva=None):
     if porcentaje_iva is None:
         porcentaje_iva = 21
@@ -7,9 +8,12 @@ def calc_iva(cant_sniva, porcentaje_iva=None):
     return total
 
 cant_sniva = 100
-total_factura = calc_iva(cant_sniva)
-print(f"El total de la factura con un 21% de IVA es: {total_factura}")
+total_fact = calc_iva(cant_sniva)
+print(f"El total de la factura con un 21% de IVA es: {total_fact}")
 
-porcentaje_iva_personalizado = 10
-total_factura_personalizado = calc_iva(cant_sniva, porcentaje_iva_personalizado)
-print(f"El total de la factura con un {porcentaje_iva_personalizado}% de IVA es: {total_factura_personalizado}")
+porcentaje_iva_pers = 10
+total_factura_pers = calc_iva(cant_sniva, porcentaje_iva_pers)
+print(f"El total de la factura con un {porcentaje_iva_pers}% de IVA es: {total_factura_pers}")
+
+#2
+DEF 
