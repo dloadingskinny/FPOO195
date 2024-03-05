f3eca53347c36729bf211f81c66d2fe081dda7ee
dloadingskinny <Daniel.y.g.a@hotmail.com>
2024-03-04 19:18:27 -0600

    Update pract11.py

--- practica 10/tkinter/pract11.py
+++ practica 10/tkinter/pract11.py
@@ -6,11 +6,11 @@ Ventana.title("ejemplo con 3 frames")
 Ventana.geometry("600x400")
 
 #2. colocamos frames de la ventana
-seccion1 =Frame(Ventana, bg="red")
+seccion1 =Frame(Ventana, bg="Black")
 seccion1.pack(expand= True, fill='both')
-seccion2 = Frame(Ventana, bg="yellow")
+seccion2 = Frame(Ventana, bg="white")
 seccion2.pack()
-seccion3 = Frame(Ventana, bg="black")
+seccion3 = Frame(Ventana, bg="Gray")
 seccion3.pack()
 #ejecuta la ventana
 Ventana.mainloop()
