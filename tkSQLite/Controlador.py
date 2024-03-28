4fce32678e8d9c64cbeee05013d40272e95042a3
dloadingskinny <Daniel.y.g.a@hotmail.com>
2024-03-27 19:59:55 -0600

    practica 17

--- tkSQLite/Controlador.py
+++ tkSQLite/Controlador.py
@@ -10,37 +10,36 @@ class Controlador:
             return conex
         except sqlite3.OperationalError:
             print("No se puede conectar")
-            
+
     def encriptapass(self,cont):
         passPlana = cont
         PassPlana = passPlana.encode()
         sal = bcrypt.gensalt()
         passHash = bcrypt.hashpw(PassPlana,sal)
-        
+
         return passHash
-    
+
     def insertUsuario(self,nom,corr,cont):
         conexion=self.conexion()
-        
+
         if(nom=="" or corr=="" or cont==""):
             messagebox.showwarning("Cuidado","Inputs vacios")
             conexion.close()
-            
+
         else:
             cursor = conexion.cursor()
             conH=self.encriptapass(cont)
             datos = (nom,corr,conH)
             sqlInsert ="insert into tbUsuarios(nombre,correo,contrasena) values(?,?,?)"
-            
-            
+
             cursor.execute(sqlInsert,datos)
             conexion.commit()
             conexion.close()
             messagebox.showinfo("Exito")
-            
+
     def buscarUsuario(self,id):
         conex=self.conexion()
-        
+
         if(id==""):
             messagebox.showwarning("Tus inputs estan vacios")
             conex.close()
@@ -54,6 +53,19 @@ class Controlador:
                 return usuario
             except sqlite3.OperationalError:
                 print("Nose puede ejcutar la busqueda")
-        
+
+    def mostrarTodosUsuarios(self):
+        conex = self.conexion()
+        cursor = conex.cursor()
+        try:
+            cursor.execute("SELECT * FROM tbUsuarios")
+            usuarios = cursor.fetchall()
+            conex.close()
+            return usuarios
+        except sqlite3.OperationalError:
+            print("No se puede ejecutar la consulta")
+            conex.close()
+            return []
+
     
         
--- tkSQLite/notebookCrud.py
+++ tkSQLite/notebookCrud.py
@@ -1,7 +1,8 @@
-from  tkinter import *
+from tkinter import *
 from tkinter import ttk
 import tkinter as tk
-from Controlador import *
+from Controlador import Controlador
+from tkinter import messagebox
 
 objControlador = Controlador()
 
@@ -13,34 +14,42 @@ def busUsuario():
     if usuarioBD==[]:
         messagebox.showwarning("Nada","id no existe")
     else:
-        print(usuarioBD)    
-    
-    
+        print(usuarioBD)
 
-#1 crear la ventana
+def mostrarUsuarios():
+    usuarios = objControlador.mostrarTodosUsuarios()
+    texto_usuarios = ''
+    for usuario in usuarios:
+        texto_usuarios += f"ID: {usuario[0]}, Nombre: {usuario[1]}, Correo: {usuario[2]}, Contrasena: {usuario[3]}\n"
+    
+    # Limpia el área de texto antes de insertar los nuevos datos
+    text_area.delete(1.0, "end")
+    text_area.insert("end", texto_usuarios)
+
+#1 Crear la ventana
 Ventana = Tk()
 Ventana.title("CRUD de usuarios")
 Ventana.geometry("500x300")
 
-#2. preparar el notebook para pestañas
+#2. Preparar el notebook para pestañas
 panel=ttk.Notebook(Ventana)
 panel.pack(fill='both', expand="yes")
 
-#3. definir las pestañas de notebook
+#3. Definir las pestañas de notebook
 pestana1=ttk.Frame(panel)
 pestana2=ttk.Frame(panel)
 pestana3=ttk.Frame(panel)
 pestana4=ttk.Frame(panel)
 pestana5=ttk.Frame(panel)
 
-#4 agragamos las pestañas
+#4 Agregamos las pestañas al panel
 panel.add(pestana1,text="Crear Usuario")
 panel.add(pestana2,text="Buscar Usuario")
 panel.add(pestana3,text="Consultar Usuarios")
 panel.add(pestana4,text="Editar Usuario")
 panel.add(pestana5,text="Eliminar Usuario")
 
-#5. pestaña 1: Formulario de Insert 
+#5. Pestaña 1: Formulario de Insert
 Label(pestana1, text="Registro de Usuarios",fg="blue", font=("Modern",18)).pack()
 
 var1= tk.StringVar()
@@ -57,33 +66,22 @@ Entry(pestana1,textvariable=var3).pack()
 
 Button(pestana1, text="Guardar usuario", command=ejecutaInsert).pack()
 
-#6.pestaña 2:BUSCAR USUARIO
+#6. Pestaña 2: BUSCAR USUARIO
 Label(pestana2, text="Buscar Usuario",fg="red", font=("Mono",18)).pack()
 
 varBus= tk.StringVar()
-Label(pestana2,text="Nombre:").pack()
+Label(pestana2,text="ID:").pack()
 Entry(pestana2,textvariable=varBus).pack()
 
-Button(pestana2, text="Consultar usuario", command=busUsuario ).pack()
+Button(pestana2, text="Consultar usuario", command=busUsuario).pack()
 
-Label(pestana2, text="Registrados:",fg="blue", font=("Mono",16)).pack()
-tk.Text(pestana2, height=5, width=52).pack()
-
-#7 Pestaña 3:Consultar Usuario
+#7. Pestaña 3: CONSULTAR USUARIOS
 Label(pestana3, text="Consultar Usuarios", fg="green", font=("Arial", 18)).pack()
 
-Button(pestana3, text="Mostrar usuarios").pack()
+Button(pestana3, text="Mostrar usuarios", command=mostrarUsuarios).pack()
 
 Label(pestana3, text="Listado de Usuarios:", fg="black", font=("Arial", 16)).pack()
-tk.Text(pestana3, height=5, width=52).pack()
-
-
-
-
-
-
+text_area = tk.Text(pestana3, height=5, width=52)
+text_area.pack()
 
 Ventana.mainloop()
-
-
-
